4df3b324ecb9749f027f48265054378aead1bace
Bryce Pittman <pittsoccer101@gmail.com>
2025-07-27 22:35:33 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ $.ajax({
 					<div class="card-body">
 						<h5 class="card-title">${post.title}</h5>
 						<p class="card-text">${post.post}</p>
-						<p class="card-text"><small class="text-muted">Posted on ${post.date}</small></p>
+						<p class="card-text"><small class="text-muted">Posted on ${post.datePosted}</small></p>
 					</div>
 				</div>
 				`;
